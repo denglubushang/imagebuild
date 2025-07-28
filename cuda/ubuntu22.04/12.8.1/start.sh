a98e614eba8516656cbace3593d9e4e8ad3560a6
ljf <2386144785@qq.com>
2025-07-28 14:58:59 +0800

    add cuda12.6 12.8

---
+++ cuda/ubuntu22.04/12.8.1/start.sh
@@ -0,0 +1,26 @@
+if [ "$JUPYTER_PASSWORD" == "" ]; then
+    JUPYTER_PASSWORD="1234567890"
+fi
+
+if [ "$JUPYTER_PORT" == "" ]; then
+    JUPYTER_PORT="8888"
+fi
+
+if [ "$JUPYTER_PREFERRED_DIR" == "" ]; then
+    JUPYTER_PREFERRED_DIR="/root"
+fi
+
+if [ "$JUPYTER_SHELL" == "" ]; then
+    JUPYTER_SHELL="/bin/bash"
+fi
+
+exec jupyter lab \
+    --allow-root \
+    --no-browser \
+    --port="$JUPYTER_PORT" \
+    --ip=* \
+    --FileContentsManager.delete_to_trash=False \
+    --ServerApp.terminado_settings='{"shell_command":["'"$JUPYTER_SHELL"'"]}' \
+    --ServerApp.token="$JUPYTER_PASSWORD" \
+    --ServerApp.allow_origin=* \
+    --ServerApp.preferred_dir="$JUPYTER_PREFERRED_DIR"
